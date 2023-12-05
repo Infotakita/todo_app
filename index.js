140810c4da6e021ded88464aa3fc78d4ac023cd0
Infotakita <infotakita@gmail.com>
2023-12-05 10:57:32 -0300

    iniciando o filtro de tarefas ativas

--- index.js
+++ index.js
@@ -74,6 +74,12 @@ app.post('/criar', (requisicao, resposta) => {
 })
 })
 
+app.get('/ativas', (requisicao, resposta)=>{
+
+
+    
+})
+
 app.get('/', (requisicao, resposta) => {
     const sql = 'SELECT * FROM tarefas';
 
